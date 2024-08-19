64cf9ffa29dc547a65bed89ffbe7cd5b849151cd
DinoDan45 <mcabezas@correo.iue.edu.co>
2024-08-18 22:59:37 -0500

    primer paso

--- App.js
+++ App.js
@@ -4,7 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.containerCubo1}>
+        <Text>¡Bienvenidos Jugadores!</Text>
+      </View>
+      <View style={styles.containerCubo2}>
+        <Text>Transmisión en vivo</Text>
+      </View>
+      <View style={styles.containerCubo3}>
+        <Text>¡Suscribete!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'plum',
+    padding: 10,
+  },
+
+  containerCubo1: {
+    height: 100,
+    backgroundColor: 'burlywood',
+    marginBottom: 10,
+    
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  containerCubo2: {
+    flex: 1,
+    height: 100,
+    backgroundColor: 'aqua',
+    marginBottom: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  containerCubo3: {
+    flexDirection: "column-reverse",
+    height: 100,
+    backgroundColor: 'burlywood',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
